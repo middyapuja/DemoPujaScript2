57af6fe51af234edc1c57f3d7851b665998d41bc
PUJA MIDDYA <PUJA MIDDYA@DESKTOP-7Q6STK1.mshome.net>
2024-10-05 13:20:34 +0530

    pushing

---
+++ DemoPujaScript2/src/test/java/contactTest/CreateContactTest.java
@@ -0,0 +1,8 @@
+package contactTest;
+
+public class CreateContactTest {
+	public static void main(String[] args) {
+		System.out.println("contact");
+	}
+
+}
---
+++ DemoPujaScript2/src/test/java/organizationTest/CreateOrgTest.java
@@ -0,0 +1,10 @@
+package organizationTest;
+
+public class CreateOrgTest {
+
+	public static void main(String[] args) {
+		System.out.println("org");
+
+	}
+
+}
