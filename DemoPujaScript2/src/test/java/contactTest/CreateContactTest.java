b0fb50730fb8c96fb34eebc03f9f9bfa38f1bf40
LENOVO <LENOVO@DESKTOP-SN4Q53J>
2024-10-05 13:34:48 -1200

    First Push Success

--- DemoPujaScript2/src/test/java/contactTest/CreateContactTest.java
+++ DemoPujaScript2/src/test/java/contactTest/CreateContactTest.java
@@ -3,6 +3,7 @@ package contactTest;
 public class CreateContactTest {
 	public static void main(String[] args) {
 		System.out.println("contact");
+		System.out.println("Contacts created sucessfully");
 	}
 
 }
---
+++ DemoPujaScript2/src/test/java/leadsPackage/TocreateLeads.java
@@ -0,0 +1,11 @@
+package leadsPackage;
+
+public class TocreateLeads {
+
+	public static void main(String[] args) {
+		System.out.println("Leads");
+ 
+		System.out.println("Created leads successfully");
+	}
+
+}
